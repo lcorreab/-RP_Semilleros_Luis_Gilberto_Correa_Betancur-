877249657e6868a4714eed7f9147dee58dd69ff2
luis-correaB <lcob31@outlook.com>
2019-10-31 20:33:52 -0500

    Rq_Semillero_Ejercicio_Devops_v.10
    V1.0 aplicacion Devops -Linea base

---
+++ Semillero_2019_Ejercicio_Devops/Semillero_2019_Ejercicio_Devops/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Semillero_2019_Ejercicio_Devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
